f46f81418e4956ef06fbb5ce1093630478d80aa8
kumars947 <kumars090407@gmail.com>
2026-09-10 11:24:17 +0530

    Add Card component with user information display

--- Src/Components/Card.jsx
+++ Src/Components/Card.jsx
@@ -1,1 +1,17 @@
 
+export function Card(){
+
+    return <div className="h-[300px] w-[500px] border border-blue-800 m-2 rounded-xl bg-gray-300">
+        <div className="text-4xl text-center"> About</div>
+        <div className="flex justify-between mr-2 ml-2">
+            <div>
+                <div className="text-2xl">Anand Kumar</div>
+                <div className="text-2xl">KEC, Katihar</div>
+                <div className="text-2xl">25105129023</div>
+            </div>
+            
+            <div className="h-[100px] w-[100px] bg-yellow-500 rounded-full"></div>
+        </div>
+        
+    </div>
+}
