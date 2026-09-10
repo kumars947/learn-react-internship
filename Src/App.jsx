684b947610016959db535a6228892c9a87dd8320
kumars947 <kumars090407@gmail.com>
2026-09-10 11:26:48 +0530

    Implement counter functionality in App component
    Added state management for a counter and imported components.

--- Src/App.jsx
+++ Src/App.jsx
@@ -1,1 +1,25 @@
+import { useState } from "react";
+import { Button } from "./components/Button";
+import { Welcome } from "./components/Welcome";
+import { Card } from "./components/Card";
 
+
+
+function App(){
+  let [count, setCount] = useState(0)
+  function counter(){
+    setCount(count = count + 1)
+    console.log(count);
+  }
+
+  return <div>
+    {/* <button className="text-green-500" onClick={counter}>click</button>
+    <div>
+      counter {count}
+    </div> */}
+    <Card></Card>
+  </div>
+
+}
+
+export default App;
